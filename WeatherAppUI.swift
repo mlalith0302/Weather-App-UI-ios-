87e4b2087711356ea664fdc64c36d191b0eb1a2d
M Sri Lalith <122102719+mlalith0302@users.noreply.github.com>
2023-03-12 11:35:16 +0530

    Update WeatherAppUI.swift

--- WeatherAppUI.swift
+++ WeatherAppUI.swift
@@ -151,3 +151,20 @@ struct MainWeatherStatusView: View {
         }
     }
 }
+
+struct WeatherButton: View {
+    
+    var title: String    
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 160, height: 50)
+            .background(isNight ? .white : .black)
+            .foregroundColor(isNight ? .black : .white)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(100)
+    }
+    
+}
+
